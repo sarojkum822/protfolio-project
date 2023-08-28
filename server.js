c47057ffe709ec91bfa7bb12ef21319349125932
Saroj kumar <sarojkum822@gmail.com>
2023-08-28 20:34:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const path = require('path')
-const routes = requiure('./routes')
+const routes = require('./routes')
 //dotenv configuartion
 dotenv.config();
 
